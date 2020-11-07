4af256bb50801b42a66643b902595d9bb379e5b6
jspw <mhshifat757@gmail.com>
2020-11-07 23:07:33 +0600

    mobile login page

--- client/Mobile/medone/lib/Admin/Screens/login.dart
+++ client/Mobile/medone/lib/Admin/Screens/login.dart
@@ -39,71 +39,115 @@ class LoginState extends State {
               ),
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              CustomTextField(
-                _usernameController,
-                "Username",
-                Icon(
-                  Icons.person,
-                  color: Colors.black,
+          Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 15,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CustomTextField(
-                _passwordController,
-                "Password",
-                Icon(Icons.security, color: Colors.black),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Checkbox(
-                            value: _rembermeValue,
-                            onChanged: (_rememberValue) {
-                              setState(() {
-                                _rembermeValue = !_rembermeValue;
-                              });
-                            }),
-                        Text(
-                          "Remember Me",
-                          style: TextStyle(fontSize: 20.0, color: Colors.black),
-                        )
-                      ],
-                    ),
-                    FlatButton(
-                      onPressed: null,
-                      child: Text(
-                        "Forget Password ?",
-                        style: TextStyle(fontSize: 20.0, color: Colors.black),
+                CustomTextField(
+                  _usernameController,
+                  "Username",
+                  Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CustomTextField(
+                  _passwordController,
+                  "Password",
+                  Icon(Icons.security, color: Colors.black),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Checkbox(
+                              value: _rembermeValue,
+                              onChanged: (_rememberValue) {
+                                setState(() {
+                                  _rembermeValue = !_rembermeValue;
+                                });
+                              }),
+                          Text(
+                            "Remember Me",
+                            style:
+                                TextStyle(fontSize: 20.0, color: Colors.black),
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ),
-              )
-            ],
+                      FlatButton(
+                        onPressed: null,
+                        child: Text(
+                          "Forget Password ?",
+                          style: TextStyle(fontSize: 20.0, color: Colors.black),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Container(
               color: Theme.of(context).accentColor,
               child: FlatButton(
-                  onPressed: () => debugPrint("Login"),
+                  onPressed: () {
+                    debugPrint("Printing Login Info : ");
+                    debugPrint(_usernameController.toString());
+                    debugPrint(_passwordController.toString());
+                    debugPrint(_rembermeValue.toString());
+                    debugPrint("Login");
+                  },
                   child: Text(
                     "Login",
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 20.0,
+                      fontSize: 24.0,
                     ),
                   )),
+            ),
+          ),
+          Center(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "or ",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500),
+                ),
+                GestureDetector(
+                  child: Text(
+                    " Signup ",
+                    style: TextStyle(
+                        color: Theme.of(context).accentColor,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  onTap: () => debugPrint("Signup"),
+                ),
+                Text(
+                  "Using",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500),
+                ),
+              ],
             ),
           )
         ],
