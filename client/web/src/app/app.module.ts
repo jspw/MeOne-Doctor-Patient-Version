c9d8497685b2a168aee5c57ee9324b658be030be
FAZLE RAKIB <fazle30@student.sust.edu>
2020-11-07 18:56:21 +0500

    Navbar and sidenav created

---
+++ client/web/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientComponent,
+    DoctorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/web/src/app/material/material.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge'; 
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {FormsModule} from '@angular/forms';
+import {MatMenuModule} from '@angular/material/menu'; 
+import {MatListModule} from '@angular/material/list'
+const MaterialComponents = [MatButtonModule,
+                            MatButtonToggleModule,
+                            MatIconModule,
+                            MatBadgeModule,
+                            MatProgressSpinnerModule,
+                            MatToolbarModule,
+                            MatSidenavModule,
+                            FormsModule,
+                            MatMenuModule,
+                            MatListModule];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialComponents
+  ],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
