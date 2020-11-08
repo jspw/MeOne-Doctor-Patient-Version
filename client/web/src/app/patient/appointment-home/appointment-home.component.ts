fb0df428ae8e069c3d26f8acfb9baa78fb1d39ab
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2020-11-09 01:12:40 +0600

    Appointment Home Created Incomplete

---
+++ client/web/src/app/patient/appointment-home/appointment-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-home',
+  templateUrl: './appointment-home.component.html',
+  styleUrls: ['./appointment-home.component.css']
+})
+export class AppointmentHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
