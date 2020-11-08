e8631e1f4641e577cd6a6fa7149fc8a2067d82ee
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2020-11-08 13:55:21 +0600

    Code Formatted

--- client/web/src/app/patient/patient.module.ts
+++ client/web/src/app/patient/patient.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material.module';
 
 import { PatientRoutingModule } from './patient-routing.module';
 import { PatientComponent } from './patient.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { MaterialModule } from '../material.module';
+import { FormModalComponent } from './form-modal/form-modal.component';
+import { FormComponent } from './form-modal/form/form.component';
 
 
 @NgModule({
   declarations: [
     PatientComponent,
-      NavbarComponent],
+    NavbarComponent,
+    FormModalComponent,
+    // FormComponent
+  ],
+  entryComponents: [
+    FormComponent
+  ],
   imports: [
     CommonModule,
     PatientRoutingModule,
