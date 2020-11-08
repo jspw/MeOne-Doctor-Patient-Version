2ea2bd9934c4b12d61626212ccd4c5fdb7ddb965
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2020-11-08 13:54:40 +0600

    Angular Material Module Update

--- client/web/src/app/material.module.ts
+++ client/web/src/app/material.module.ts
@@ -6,57 +6,58 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { CommonModule } from '@angular/common';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatIconModule} from '@angular/material/icon';
-import {MatBadgeModule} from '@angular/material/badge'; 
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
-import {MatToolbarModule} from '@angular/material/toolbar'; 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {FormsModule} from '@angular/forms';
-import {MatMenuModule} from '@angular/material/menu'; 
-import {MatListModule} from '@angular/material/list'
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list'
+import { MatDialogModule } from '@angular/material/dialog';
 
-const MaterialComponents = [];
 
 @NgModule({
     imports: [
         MatCardModule,
-                            MatButtonModule,
-                            MatInputModule,
-                            MatRadioModule,
-                            MatTableModule,
-                            MatPaginatorModule,
-                            MatSortModule,
-                            MatButtonModule,
-                            MatButtonToggleModule,
-                            MatIconModule,
-                            MatBadgeModule,
-                            MatProgressSpinnerModule,
-                            MatToolbarModule,
-                            MatSidenavModule,
-                            FormsModule,
-                            MatMenuModule,
-                            MatListModule
+        MatButtonModule,
+        MatInputModule,
+        MatRadioModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatBadgeModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        FormsModule,
+        MatMenuModule,
+        MatListModule,
+        MatDialogModule,
     ],
     exports: [
-                             MatCardModule,
-                            MatButtonModule,
-                            MatInputModule,
-                            MatRadioModule,
-                            MatTableModule,
-                            MatPaginatorModule,
-                            MatSortModule,
-                            MatButtonModule,
-                            MatButtonToggleModule,
-                            MatIconModule,
-                            MatBadgeModule,
-                            MatProgressSpinnerModule,
-                            MatToolbarModule,
-                            MatSidenavModule,
-                            FormsModule,
-                            MatMenuModule,
-                            MatListModule
+        MatCardModule,
+        MatButtonModule,
+        MatInputModule,
+        MatRadioModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatBadgeModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        FormsModule,
+        MatMenuModule,
+        MatListModule,
+        MatDialogModule,
     ]
 })
 export class MaterialModule { }
